c0a8c9528faaa27bd4c06174ce05a36b99e4566b
evandrotvc <evandro-tvc@hotmail.com>
2022-10-10 21:21:52 -0300

    feat: test em teams

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
@@ -8,10 +7,11 @@ RSpec.describe Team, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
-    it { should validate_uniqueness_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
 
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
     it { is_expected.to validate_length_of(:description).is_at_most(255) }
+    it { is_expected.to have_one(:photo_attachment) }
   end
 
   describe 'team create' do
