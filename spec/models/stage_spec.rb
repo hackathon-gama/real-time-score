b7b04f141c69dcc3d63815582ac369fb4c6f91f1
Weder Cabral Mendes <wederc@gmail.com>
2022-10-11 17:49:24 -0300

    lint: Corrige erros do rubocop

--- spec/models/stage_spec.rb
+++ spec/models/stage_spec.rb
@@ -3,27 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Stage, type: :model do
-  subject(:stage) { build(:stage) }
+  let(:stage) { build(:stage) }
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
 
     it do
-        should define_enum_for(:name).
-          with_values(
-            groups: 'groups',
-            octave: 'octave',
-            fourth_final: 'fourth_final',
-            semi: 'semi',
-            final: 'final'
-          ).
-          backed_by_column_of_type(:string)
-      end
+      expect(stage).to define_enum_for(:name)
+        .with_values(
+          groups: 'groups',
+          octave: 'octave',
+          fourth_final: 'fourth_final',
+          semi: 'semi',
+          final: 'final'
+        )
+        .backed_by_column_of_type(:string)
+    end
   end
 
   describe 'stage create' do
     before { stage.save }
 
-    it { is_expected.to be_persisted }
+    it { expect(stage).to be_persisted }
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Team, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
-    it { should have_db_index(:name).unique }
+    it { is_expected.to have_db_index(:name).unique }
 
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
     it { is_expected.to validate_length_of(:description).is_at_most(255) }
