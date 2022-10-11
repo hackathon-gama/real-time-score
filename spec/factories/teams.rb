cf4e1b35278edb4f99d22c733ba096ca7eb3d1d0
evandrotvc <evandro-tvc@hotmail.com>
2022-10-10 21:21:22 -0300

    fix: ajusta factory e insere uma imagem faker para o upload no factory

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :team do
-    name { Faker::Name.name_with_middle  }
+    name { Faker::Name.name_with_middle }
     description { Faker::Lorem.sentence }
-    photo { Faker::Avatar.image }
+
+    photo do
+      Rack::Test::UploadedFile.new('spec/fixtures/files/photo.png', 'image/png')
+    end
   end
 end
