034e6c1ce3874b41f0603699c239bb9848986570
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2022-10-11 09:55:11 -0300

    Correção em factory de usuário

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :user do
     full_name { Faker::Name.name_with_middle }
     email { Faker::Internet.email }
-    password_digest { SecureRandom.hex }
+    password_digest { Faker::Internet.password }
   end
 end
