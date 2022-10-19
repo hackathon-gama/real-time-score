5c21f2f47574193cb8b30c2a55997d7674034cce
evandrotvc <evandro-tvc@hotmail.com>
2022-10-19 20:36:22 -0300

    corrige db schema

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_004353) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_teams_on_name", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
