54eafd90944c8e975b228b7b2b3a96759cd4d566
Weder Cabral Mendes <wederc@gmail.com>
2022-10-20 18:08:49 -0300

    feat: Adiciona directs_uploads e seus testes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :direct_uploads, only: :create
+    end
+  end
 end
