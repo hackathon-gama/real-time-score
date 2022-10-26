ddbb41d8cd7dc1a646a608901b00be892b9073e4
evandrotvc <evandro-tvc@hotmail.com>
2022-10-25 21:11:34 -0300

    fix: retira index teams

--- db/migrate/20221018234501_add_references_team_to_matches.rb
+++ db/migrate/20221018234501_add_references_team_to_matches.rb
@@ -3,6 +3,6 @@ class AddReferencesTeamToMatches < ActiveRecord::Migration[7.0]
     add_reference :matches, :team_away, index: true
     add_reference :matches, :team_home, index: true
     add_index :matches, %i[stage_id team_away_id team_home_id], unique: true
-    add_index :teams, :name, unique: true
+    # add_index :teams, :name, unique: true
   end
 end
