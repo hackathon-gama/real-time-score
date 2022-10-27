f5c4d2a01a4a7eea5e9079b93deeb1528c650655
lucas araujo <lucas.trabalho.araujo@gmail.com>
2022-10-26 22:02:48 -0300

    criado concern

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,36 +2,7 @@
 
 class Api::V1::ApplicationController < ActionController::API
   include ExceptionHandler
+  include AuthenticationHandler
 
   before_action :authorize
-
-  def encode_token(payload)
-    JWT.encode(payload, SECRET_KEY)
-  end
-
-  def decode_token
-    auth_header = request.headers['Authorization']
-
-    return unless auth_header
-
-    token = auth_header.split.last
-    begin
-      JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256')
-    rescue JWT::DecodeError
-      nil
-    end
-  end
-
-  def current_user
-    decoded_token = decode_token
-
-    return unless decoded_token
-
-    user_id = decoded_token.first['user_id']
-    @user = User.find_by(id: user_id)
-  end
-
-  def authorize
-    render json: { message: 'Please log in' }, status: :unauthorized unless current_user
-  end
 end
