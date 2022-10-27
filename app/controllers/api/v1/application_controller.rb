666800e57ce8eae50909a3e542ff2a43de5fb85c
lucas araujo <lucas.trabalho.araujo@gmail.com>
2022-10-26 21:34:53 -0300

    adicionados metodos de autorizacao no application controller

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,4 +2,34 @@
 
 class Api::V1::ApplicationController < ActionController::API
   include ExceptionHandler
+
+  before_action :authorize
+
+  def encode_token(payload)
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decode_token
+    auth_header = request.headers['Authorization']
+    if auth_header
+      token = auth_header.split(' ').last
+      begin
+        JWT.decode(token,  SECRET_KEY, true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    decoded_token = decode_token
+    if decoded_token
+      user_id = decoded_token.first['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def authorize
+    render json: { message: 'Please log in' }, status: :unauthorized unless current_user
+  end
 end
