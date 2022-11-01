aa65e03bdb21d1d9ee86e74fac98e2f106c86c5d
lucas araujo <lucas.trabalho.araujo@gmail.com>
2022-10-31 21:29:35 -0300

    alterado fetch env secret key no auth_headers_context.rb

--- spec/contexts/auth_headers_context.rb
+++ spec/contexts/auth_headers_context.rb
@@ -2,6 +2,6 @@
 
 shared_context 'with auth headers' do
   let(:user) { create(:user) }
-  let(:token) { JWT.encode({ user_id: user.id }, ENV.fetch('SECRET_KEY', nil)) }
+  let(:token) { JWT.encode({ user_id: user.id }, ENV.fetch('SECRET_KEY', 'secret')) }
   let(:auth_headers) { { 'Authorization' => "Bearer #{token}" } }
 end
