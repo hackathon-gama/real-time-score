5210ba20ff3895e16164bc5eb3d1acdd20a58ac1
Weder Cabral Mendes <wederc@gmail.com>
2022-11-02 17:43:54 -0300

    feat: Add file_extension no FileImportManager

--- spec/models/file_import_manager_spec.rb
+++ spec/models/file_import_manager_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe FileImportManager, type: :model do
     it { is_expected.to have_one_attached(:file) }
   end
 
+  describe 'file_import_manager create' do
+    before { file_import_manager.save }
+
+    it { is_expected.to be_persisted }
+  end
+
   describe 'aasm state machine' do
     context 'when status is pending' do
       it { is_expected.to have_state(:pending) }
@@ -57,9 +63,18 @@ RSpec.describe FileImportManager, type: :model do
     end
   end
 
-  describe 'file_import_manager create' do
-    before { file_import_manager.save }
+  describe '#file_extension' do
+    it 'will return the file extension' do
+      file = fixture_file_upload('csv_with_semicolon.csv')
+      file_import_manager = create(:file_import_manager, file: file)
 
-    it { is_expected.to be_persisted }
+      expect(file_import_manager.file_extension).to eq('csv')
+    end
+
+    it 'will return null without file' do
+      file_import_manager = create(:file_import_manager)
+
+      expect(file_import_manager.file_extension).to be_nil
+    end
   end
 end
