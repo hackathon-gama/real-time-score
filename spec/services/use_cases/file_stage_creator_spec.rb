2f543bb2300ed591ba024dd18bf55493aaa23bad
evandrotvc <evandro-tvc@hotmail.com>
2022-11-06 14:01:54 -0300

    feat: ajusta testes unitários

--- spec/services/use_cases/file_stage_creator_spec.rb
+++ spec/services/use_cases/file_stage_creator_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe UseCases::FileStageCreator do
   context 'when pass correct permited_attributes' do
-    byebug
     let(:file_extractor) { instance_double(BaseExtractor) }
 
     before do
@@ -15,33 +14,17 @@ RSpec.describe UseCases::FileStageCreator do
 
     it 'will create stages' do
       attributes = %w[name]
-      byebug
+
       expect { described_class.call(file_extractor, permited_attributes: attributes) }
         .to change(Stage, :count).by(2)
     end
 
     it 'will not set unspecified permited_attributes' do
-      attributes = %w[name]
+      attributes = %w[]
 
       described_class.call(file_extractor, permited_attributes: attributes)
 
-      expect(Stage.last.name).to be_nil
-    end
-  end
-
-  context 'when dont pass correct permited_attributes' do
-    let(:file_extractor) { instance_double(BaseExtractor) }
-
-    before do
-      allow(file_extractor).to receive(:execute)
-        .and_yield({ name: 'semi' })
-    end
-
-    it 'raise ActiveRecord::RecordInvalid error without required permited_attributes' do
-      attributes = %w[]
-
-      expect { described_class.call(file_extractor, permited_attributes: attributes) }
-        .to raise_error(ActiveRecord::RecordInvalid)
+      expect(Stage.count).to eq(2)
     end
   end
 end
