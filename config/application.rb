99b125d2b8dfd15777a4c31cde5a24383f02b401
Weder Cabral Mendes <wederc@gmail.com>
2022-11-16 19:06:00 -0300

    fix: SQS apenas em Prod e Development

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,6 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.active_job.queue_adapter = :amazon_sqs
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
