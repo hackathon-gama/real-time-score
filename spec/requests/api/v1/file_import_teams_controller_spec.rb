286550c490471dcb72f4955e3dec9127692d15d8
Weder Cabral Mendes <wederc@gmail.com>
2022-11-16 19:43:16 -0300

    fix: Corrige Guardfile para rodar requests spec

--- spec/requests/api/v1/file_import_teams_controller_spec.rb
+++ spec/requests/api/v1/file_import_teams_controller_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::FileImportTeams', type: :request do
+  include_context 'with auth headers'
+
   describe 'POST /create' do
     before do
       create(:file_import_manager, file: fixture_file_upload('csv_with_semicolon.csv'))
@@ -15,6 +17,7 @@ RSpec.describe 'Api::V1::FileImportTeams', type: :request do
     it 'returns http created' do
       post api_v1_file_import_teams_path,
         params: params,
+        headers: auth_headers,
         as: :json
 
       expect(response).to have_http_status(:created)
@@ -23,6 +26,7 @@ RSpec.describe 'Api::V1::FileImportTeams', type: :request do
     it 'will enqueue FileImportTeamJob' do
       post api_v1_file_import_teams_path,
         params: params,
+        headers: auth_headers,
         as: :json
 
       expect(FileImportTeamJob).to(
