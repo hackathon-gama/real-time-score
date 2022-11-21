7d6230845846fbaa7ce3a5fedb092fe2dcc8fd7d
Weder Cabral Mendes <wederc@gmail.com>
2022-11-21 18:08:45 -0300

    Adiciona máquina de estado no status do Match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Match < ApplicationRecord
+  include AASM
+
   belongs_to :stage
   belongs_to :team_home, class_name: 'Team'
   belongs_to :team_away, class_name: 'Team'
@@ -11,4 +13,17 @@ class Match < ApplicationRecord
   validates :away_goals, numericality: { only_integer: true }
   validates :status, length: { maximum: 255 }
   validates :stage, uniqueness: { scope: %i[team_home_id team_away_id] }
+
+  aasm column: :status, requires_lock: true do
+    state :pending, initial: true
+    state :running, :finished
+
+    event :start do
+      transitions from: :pending, to: :running
+    end
+
+    event :done do
+      transitions from: :running, to: :finished
+    end
+  end
 end
